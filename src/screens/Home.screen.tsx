bd44ab358ec75e2ba0cc3ca8a4f8701ca871df2c
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2022-01-01 19:30:42 +0100

    :sparkles: JavaScript libraries

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
+import { MoodOptionType, MoodOptionWithTimestamp } from '../types';
+import { MoodItemRow } from '../components/MoodItemRow';
 
 export const Home: React.FC = () => {
+  const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
+
+  const handleSelectMood = useCallback((selectedMood: MoodOptionType) => {
+    setMoodList(current => [
+      ...current,
+      { mood: selectedMood, timestamp: Date.now() },
+    ]);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MoodPicker />
+      <MoodPicker handleSelectMood={handleSelectMood} />
+      {moodList.map(item => (
+        <MoodItemRow item={item} key={item.timestamp} />
+      ))}
     </View>
   );
 };
